fa19b11fc0f0bcbe6f91b35678a75f7a8a1ca9d7
mieburungdara <rereitureza@gmail.com>
2026-04-21 18:15:18 +0800

    build(deps): pembaruan konfigurasi vendor dan gitignore 🛠️
    Mengubah pola pengabaian direktori vendor pada `.gitignore` dan menyertakan folder `vendor/` ke dalam repositori untuk memastikan ketersediaan dependensi.

---
+++ vendor/composer/InstalledVersions.php
@@ -0,0 +1,670 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+namespace Composer;
+
+use Composer\Autoload\ClassLoader;
+use Composer\Semver\VersionParser;
+
+
+
+
+
+
+class InstalledVersions
+{
+private static $installed = array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '91254b38bac15b9749ef2f89a42b521c00d4f943',
+    'name' => 'botsawer/telegram-creator-payment',
+  ),
+  'versions' => 
+  array (
+    'botsawer/telegram-creator-payment' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '91254b38bac15b9749ef2f89a42b521c00d4f943',
+    ),
+    'carbonphp/carbon-doctrine-types' => 
+    array (
+      'pretty_version' => '2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '99f76ffa36cce3b70a4a6abce41dba15ca2e84cb',
+    ),
+    'doctrine/inflector' => 
+    array (
+      'pretty_version' => '2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6d6c96277ea252fc1304627204c3d5e6e15faa3b',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '6.5.8',
+      'version' => '6.5.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a52f0440530b54fa079ce76e8c5d196a42cad981',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.5.3',
+      'version' => '1.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '67ab6e18aaa14d753cc148911d273f6e6cb6721e',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.9.1',
+      'version' => '1.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e4490cabc77465aaee90b20cfc9a770f8c04be6b',
+    ),
+    'illuminate/collections' => 
+    array (
+      'pretty_version' => 'v8.83.27',
+      'version' => '8.83.27.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '705a4e1ef93cd492c45b9b3e7911cccc990a07f4',
+    ),
+    'illuminate/container' => 
+    array (
+      'pretty_version' => 'v8.83.27',
+      'version' => '8.83.27.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '14062628d05f75047c5a1360b9350028427d568e',
+    ),
+    'illuminate/contracts' => 
+    array (
+      'pretty_version' => 'v8.83.27',
+      'version' => '8.83.27.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5e0fd287a1b22a6b346a9f7cd484d8cf0234585d',
+    ),
+    'illuminate/database' => 
+    array (
+      'pretty_version' => 'v8.83.27',
+      'version' => '8.83.27.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1a5b0e4e6913415464fa2aab554a38b9e6fa44b1',
+    ),
+    'illuminate/macroable' => 
+    array (
+      'pretty_version' => 'v8.83.27',
+      'version' => '8.83.27.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aed81891a6e046fdee72edd497f822190f61c162',
+    ),
+    'illuminate/support' => 
+    array (
+      'pretty_version' => 'v8.83.27',
+      'version' => '8.83.27.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1c79242468d3bbd9a0f7477df34f9647dde2a09b',
+    ),
+    'irazasyed/telegram-bot-sdk' => 
+    array (
+      'pretty_version' => 'v3.9',
+      'version' => '3.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c4c8250e334e94aab7b6b4a67850e39fd5a9384b',
+    ),
+    'league/event' => 
+    array (
+      'pretty_version' => '2.3.0',
+      'version' => '2.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '062ebb450efbe9a09bc2478e89b7c933875b0935',
+    ),
+    'midtrans/midtrans-php' => 
+    array (
+      'pretty_version' => '2.6.2',
+      'version' => '2.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8ed7fc58ff1ababe675da17acf8233f4028eb3be',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '2.11.0',
+      'version' => '2.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '37308608e599f34a1a4845b16440047ec98a172a',
+    ),
+    'nesbot/carbon' => 
+    array (
+      'pretty_version' => '2.73.0',
+      'version' => '2.73.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9228ce90e1035ff2f0db84b40ec2e023ed802075',
+    ),
+    'phpoption/phpoption' => 
+    array (
+      'pretty_version' => '1.9.5',
+      'version' => '1.9.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '75365b91986c2405cf5e1e012c5595cd487a98be',
+    ),
+    'psr/clock' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e41a24703d4560fd0acb709162f73b8adfc3aa0d',
+    ),
+    'psr/clock-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.1.2',
+      'version' => '1.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '513e0666f7216c7459170d56df27dfcefe1689ea',
+    ),
+    'psr/container-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.1',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cb6ce4845ce34a8ad9e68117c10ee90a29919eba',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0 || 2.0.0 || 3.0.0',
+        1 => '1.0|2.0',
+      ),
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '408d5eafb83c57f6365a3ca330ff23aa4a5fa39b',
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'respect/validation' => 
+    array (
+      'pretty_version' => '1.1.31',
+      'version' => '1.1.31.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '45d109fc830644fecc1145200d6351ce4f2769d0',
+    ),
+    'symfony/console' => 
+    array (
+      'pretty_version' => 'v5.4.47',
+      'version' => '5.4.47.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c4ba980ca61a9eb18ee6bcc73f28e475852bb1ed',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.5.4',
+      'version' => '2.5.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '605389f2a7e5625f273b53960dc46aeaf9c62918',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.34.0',
+      'version' => '1.34.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '141046a8f9477948ff284fa65be2095baafb94f2',
+    ),
+    'symfony/polyfill-intl-grapheme' => 
+    array (
+      'pretty_version' => 'v1.34.0',
+      'version' => '1.34.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ad1b7b9092976d6c948b8a187cec9faaea9ec1df',
+    ),
+    'symfony/polyfill-intl-idn' => 
+    array (
+      'pretty_version' => 'v1.34.0',
+      'version' => '1.34.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9614ac4d8061dc257ecc64cba1b140873dce8ad3',
+    ),
+    'symfony/polyfill-intl-normalizer' => 
+    array (
+      'pretty_version' => 'v1.34.0',
+      'version' => '1.34.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3833d7255cc303546435cb650316bff708a1c75c',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.34.0',
+      'version' => '1.34.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a21eb99c6973357967f6ce3708cd55a6bec6315',
+    ),
+    'symfony/polyfill-php73' => 
+    array (
+      'pretty_version' => 'v1.34.0',
+      'version' => '1.34.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f68c03565dcaaf25a890667542e8bd75fe7e5bb',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.34.0',
+      'version' => '1.34.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dfb55726c3a76ea3b6459fcfda1ec2d80a682411',
+    ),
+    'symfony/service-contracts' => 
+    array (
+      'pretty_version' => 'v2.5.4',
+      'version' => '2.5.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f37b419f7aea2e9abf10abd261832cace12e3300',
+    ),
+    'symfony/string' => 
+    array (
+      'pretty_version' => 'v5.4.47',
+      'version' => '5.4.47.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '136ca7d72f72b599f2631aca474a4f8e26719799',
+    ),
+    'symfony/translation' => 
+    array (
+      'pretty_version' => 'v5.4.45',
+      'version' => '5.4.45.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '98f26acc99341ca4bab345fb14d7b1d7cb825bed',
+    ),
+    'symfony/translation-contracts' => 
+    array (
+      'pretty_version' => 'v2.5.4',
+      'version' => '2.5.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '450d4172653f38818657022252f9d81be89ee9a8',
+    ),
+    'symfony/translation-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '2.3',
+      ),
+    ),
+    'vlucas/phpdotenv' => 
+    array (
+      'pretty_version' => 'v4.3.0',
+      'version' => '4.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '67a491df68208bef8c37092db11fa3885008efcf',
+    ),
+    'voku/portable-ascii' => 
+    array (
+      'pretty_version' => '1.6.1',
+      'version' => '1.6.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '87337c91b9dfacee02452244ee14ab3c43bc485a',
+    ),
+  ),
+);
+private static $canGetVendors;
+private static $installedByVendor = array();
+
+
+
+
+
+
+
+public static function getInstalledPackages()
+{
+$packages = array();
+foreach (self::getInstalled() as $installed) {
+$packages[] = array_keys($installed['versions']);
+}
+
+
+if (1 === \count($packages)) {
+return $packages[0];
+}
+
+return array_keys(array_flip(\call_user_func_array('array_merge', $packages)));
+}
+
+
+
+
+
+
+
+
+
+public static function isInstalled($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (isset($installed['versions'][$packageName])) {
+return true;
+}
+}
+
+return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function satisfies(VersionParser $parser, $packageName, $constraint)
+{
+$constraint = $parser->parseConstraints($constraint);
+$provided = $parser->parseConstraints(self::getVersionRanges($packageName));
+
+return $provided->matches($constraint);
+}
+
+
+
+
+
+
+
+
+
+
+public static function getVersionRanges($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+$ranges = array();
+if (isset($installed['versions'][$packageName]['pretty_version'])) {
+$ranges[] = $installed['versions'][$packageName]['pretty_version'];
+}
+if (array_key_exists('aliases', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['aliases']);
+}
+if (array_key_exists('replaced', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['replaced']);
+}
+if (array_key_exists('provided', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['provided']);
+}
+
+return implode(' || ', $ranges);
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getVersion($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['version'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['version'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getPrettyVersion($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['pretty_version'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['pretty_version'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getReference($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['reference'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['reference'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getRootPackage()
+{
+$installed = self::getInstalled();
+
+return $installed[0]['root'];
+}
+
+
+
+
+
+
+
+public static function getRawData()
+{
+return self::$installed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function reload($data)
+{
+self::$installed = $data;
+self::$installedByVendor = array();
+}
+
+
+
+
+private static function getInstalled()
+{
+if (null === self::$canGetVendors) {
+self::$canGetVendors = method_exists('Composer\Autoload\ClassLoader', 'getRegisteredLoaders');
+}
+
+$installed = array();
+
+if (self::$canGetVendors) {
+foreach (ClassLoader::getRegisteredLoaders() as $vendorDir => $loader) {
+if (isset(self::$installedByVendor[$vendorDir])) {
+$installed[] = self::$installedByVendor[$vendorDir];
+} elseif (is_file($vendorDir.'/composer/installed.php')) {
+$installed[] = self::$installedByVendor[$vendorDir] = require $vendorDir.'/composer/installed.php';
+}
+}
+}
+
+$installed[] = self::$installed;
+
+return $installed;
+}
+}
